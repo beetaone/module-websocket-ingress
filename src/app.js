6a57b662ee77ee2ed8f6e02f442dea9a33131eb5
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-01-11 10:40:33 +0100

    Code cleanup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,62 @@
-const { initializeListener } = require('./utils/websocket')
+const WebSocket = require('ws')
+const fetch = require('node-fetch')
+const { EGRESS_URLS, WEBSOCKET_URL } = require('./config/config.js')
 
-if (require.main === module) {
-  initializeListener()
-}
+const client = new WebSocket(WEBSOCKET_URL)
+
+client.on('message', async function message(data) {
+  console.log(`Received data: ${data}`)
+  let decodedData = data
+  try {
+    decodedData = JSON.parse(data.toString())
+  } catch (e) {
+    decodedData = data.toString()
+  }
+  const payload = {
+    timestamp: Date.now(),
+    data: decodedData,
+  }
+  if (EGRESS_URLS) {
+    const eUrls = EGRESS_URLS.replace(/ /g, '')
+    const urls = eUrls.split(',')
+    console.log({
+      urls,
+      eUrls,
+    })
+    await Promise.all(
+      urls.map(async url =>
+        fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(payload),
+        }).then(res => {
+          console.log(`Response from ${url}: ${res.status}`)
+        })
+      )
+    )
+      .then(() => {
+        console.log('Data sent to egress')
+      })
+      .catch(e => {
+        console.error(e.message)
+      })
+  } else {
+    console.error('EGRESS_URLS is not provided.')
+  }
+})
+
+client.on('open', () => {
+  // TODO: Add subscriptions to ws ingress
+  console.log('Connection opened')
+  // const subscribeMessage = JSON.stringify({
+  //   event: 'bts:subscribe',
+  //   data: {
+  //     channel: 'live_trades',
+  //   },
+  // })
+  // client.send(subscribeMessage)
+})
+
+client.on('close', () => {
+  console.log('Connection closed')
+})
--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@ const env = require('../utils/env')
 
 module.exports = {
   EGRESS_URLS: env('EGRESS_URLS', ''),
-  WEBSOCKET_URL: env('WEBSOCKET_URL', ''),
   MODULE_NAME: env('MODULE_NAME', 'Websocket Ingress'),
+  WEBSOCKET_URL: env('WEBSOCKET_URL', ''),
 }
